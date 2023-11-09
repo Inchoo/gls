40e8d0c61131edc2329e353ba7b28cf7bfc9f287
Ivan Matozan <ivan.matozan@inchoo.net>
2023-11-09 13:46:20 +0100

    Add PSD data to shipment pdf

---
+++ Plugin/Sales/Controller/Adminhtml/Shipment/PrintActionPlugin.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GLSCroatia\Shipping\Plugin\Sales\Controller\Adminhtml\Shipment;
+
+use Magento\Sales\Model\Order\Pdf\Shipment;
+
+class PrintActionPlugin
+{
+    /**
+     * @var \GLSCroatia\Shipping\ViewModel\ParcelShopDelivery
+     */
+    protected \GLSCroatia\Shipping\ViewModel\ParcelShopDelivery $parcelShopDelivery;
+
+    /**
+     * @param \GLSCroatia\Shipping\ViewModel\ParcelShopDelivery $parcelShopDelivery
+     */
+    public function __construct(\GLSCroatia\Shipping\ViewModel\ParcelShopDelivery $parcelShopDelivery)
+    {
+        $this->parcelShopDelivery = $parcelShopDelivery;
+    }
+
+    /**
+     * Add GLS parcel shop delivery data to the shipment PDF.
+     *
+     * @param \Magento\Sales\Model\Order\Pdf\Shipment $subject
+     * @param \Magento\Sales\Model\Order\Shipment[] $result
+     * @return array
+     */
+    public function beforeGetPdf(Shipment $subject, array $result): array
+    {
+        foreach ($result as $shipment) {
+            $order = $shipment->getOrder();
+
+            if (!$this->parcelShopDelivery->isParcelShopDeliveryMethod($order->getShippingMethod())
+                || !$deliveryPointData = $this->parcelShopDelivery->getParcelShopDeliveryPointData($order)
+            ) {
+                continue;
+            }
+
+            $shippingDescription =  $order->getShippingDescription();
+            $parcelShopId = $deliveryPointData['id'] ?? '';
+            if (!str_contains($shippingDescription, $parcelShopId)) {
+                $order->setShippingDescription("{$shippingDescription} {$parcelShopId}");
+            }
+        }
+
+        return [$result];
+    }
+}
