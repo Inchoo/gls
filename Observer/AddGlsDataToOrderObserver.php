efe823bceaa40a5fd54462367c7d865ad7729731
Ivan Matozan <ivan.matozan@inchoo.net>
2023-11-09 10:33:25 +0100

    Add GSL parcel shop delivery poitn data to sales emails

--- Observer/AddGlsDataToOrderObserver.php
+++ Observer/AddGlsDataToOrderObserver.php
@@ -7,7 +7,7 @@ namespace GLSCroatia\Shipping\Observer;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 
-class AddGlsDeliverLocationToOrderObserver implements ObserverInterface
+class AddGlsDataToOrderObserver implements ObserverInterface
 {
     /**
      * Save GLS delivery location to the order.
