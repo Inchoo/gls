f06bd6dc523f56081041714da22a2833385c04cf
Ivan Matozan <ivan.matozan@inchoo.net>
2024-01-10 09:41:24 +0100

    Add license details

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,4 +1,10 @@
 <?php // @codingStandardsIgnoreFile
+/**
+ * Copyright (c) 2023-present GLS Croatia. All rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ * @author Inchoo (https://inchoo.net)
+ */
 
 declare(strict_types=1);
 
--- Plugin/PayPal/Block/Express/ReviewPlugin.php
+++ Plugin/PayPal/Block/Express/ReviewPlugin.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Copyright (c) 2023-present GLS Croatia. All rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ * @author Inchoo (https://inchoo.net)
+ */
 
 namespace GLSCroatia\Shipping\Plugin\PayPal\Block\Express;
 
