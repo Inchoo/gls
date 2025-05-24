926ae137ff357200b41ec39073a3454ebe622aa9
Ivan Matozan <ivan.matozan@inchoo.net>
2025-05-24 12:51:56 +0200

    Bss_OneStepCheckout adapter (#5)
    * Bss_OneStepCheckout adapter
    
    * Improve shipping-save-processor-mixin.js

---
+++ view/frontend/web/js/checkout/model/shipping-save-processor-mixin.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2023-present GLS Croatia. All rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ * @author Inchoo (https://inchoo.net)
+ */
+
+define([
+    'jquery',
+    'Magento_Checkout/js/model/quote',
+    'GLSCroatia_Shipping/js/checkout/model/gls-data'
+], function ($, quote, glsData) {
+    'use strict';
+
+    return function (shippingSaveProcessor) {
+        var originSaveShippingInformation = shippingSaveProcessor.saveShippingInformation;
+
+        shippingSaveProcessor.saveShippingInformation = function (type) {
+            var shippingMethod = quote.shippingMethod();
+
+            if (shippingMethod
+                && shippingMethod['carrier_code'] === 'gls'
+                && (shippingMethod['method_code'] === 'locker' || shippingMethod['method_code'] === 'shop')
+            ) {
+                var deferred = $.Deferred();
+
+                glsData.shippingSaveProcessorCallback = function () {
+                    return originSaveShippingInformation.call(shippingSaveProcessor, type);
+                };
+
+                return deferred.resolve();
+            } else {
+                glsData.shippingSaveProcessorCallback = null;
+                return originSaveShippingInformation.call(shippingSaveProcessor, type);
+            }
+        };
+
+        return shippingSaveProcessor;
+    };
+});
